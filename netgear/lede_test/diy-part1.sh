d22a5e0f1ca3fc6cf52afa286f561498205fe18d
Diego <ykevin0825@gmail.com>
2025-07-17 15:05:10 +0800

    1

--- netgear/lede_test/diy-part1.sh
+++ netgear/lede_test/diy-part1.sh
@@ -15,7 +15,8 @@
 sed -i '/helloworld/d' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld;main' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld;main' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages'  >>feeds.conf.default
 #echo 'src-git smpackage https://github.com/kenzok8/small-package'  >>feeds.conf.default
